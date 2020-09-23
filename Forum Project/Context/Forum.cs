76e929bc699201aec8aac54e422607685479a654
nicocarroll99 <54688712+nicocarroll99@users.noreply.github.com>
2020-09-23 03:32:02 -0500

    Added entity framework core plugins and created the first migration for the database.

---
+++ Forum Project/Context/Forum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Forum_Project.Context
+{
+    public class ForumDbContext : DbContext 
+    { 
+        DbSet<Users> Users { get; set; }
+
+        public ForumDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
--- Forum Project/Startup.cs
+++ Forum Project/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Forum_Project.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,8 @@ namespace Forum_Project
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddDbContext<ForumDbContext>(item => item.UseSqlServer(Configuration.GetConnectionString("myconn")));
             services.AddControllersWithViews();
         }
 
